6c3b781a209d979f39cf4464242be1e9631fdae2
Jesús Casañas <casanasjesus97@gmail.com>
2025-09-26 22:06:16 -0300

    loading creating message

--- BlazorClientApp/obj/Debug/net8.0/BlazorClientApp.AssemblyInfo.cs
+++ BlazorClientApp/obj/Debug/net8.0/BlazorClientApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf0faafb88da95a2ba1cda0328345e39be5476d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ff8ecb365c86a6160dc83cbe25a83f0f7173e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
